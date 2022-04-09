0aed894f75e18106c7f950bc93e02249af68739e
Claudio <72409092+ClaudioPanConQueso@users.noreply.github.com>
2022-04-09 20:04:43 -0300

    Add files via upload

--- SCPSL/Commands/Activate.cs
+++ SCPSL/Commands/Activate.cs
@@ -20,37 +20,52 @@ namespace OmegaWarheadPlugin.Commands
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-            foreach (Room room in Room.List)
-                room.Color = Color.cyan;
-            string cassie = ("pitch_0.2 .g3 .g3 .g3 pitch_0.9 attention . attention . activating omega warhead . detonation in 3 minutes . please evacuate in helicopter of surface zone or in the breach shelter . please evacuate now . 170 seconds before destruction .g3 . .  .g3 . .  . .g3 . . . .g3 . . . .g3 . . . . .g3 . . . . .g3 . . . .g3 . . . .g3 . . . .g3 . . . .g3 . . . .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 120 seconds  .g3 . .  .g3 . .  .g3 . .  .g3 . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .  .g3 . . .   .g3 . . .   .g3 . .   .g3 . . .   .g3 . . .   .g3 . . .   .g3 . . .   .g3 . . .   .g3 . . .   .g3 . . .   .g3 . . .   .g3 . . .   .g3 . . .   .g3 . . .   1 minute .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . .g3 .  . 30 seconds . all checkpoint doors are open . please evacuate . 20 . 19 . 18 . 17 . 16 . 15 . 14 . 13 . 12 . 11 . 10 seconds 9 . 8 . 7 . 6 . 5 . 4 . 3 . 2 . 1 . pitch_0.7 0");
-            Cassie.Message(cassie, false, false);
-            Map.Broadcast(10, Plugin.Instance.Config.ActivatedMessage);
-            Timing.CallDelayed(180, () =>
+            if(sender.CheckPermission(PlayerPermissions.WarheadEvents))
             {
-                foreach (Player Sobrevivientes in Player.List.Where(plr => plr.CurrentRoom.Name == "EZ_Shelter"))
+                foreach (Room room in Room.List)
+                    room.Color = Color.cyan;
+                Cassie.Message(Plugin.Instance.Config.Cassie, false, false);
+                Map.Broadcast(10, Plugin.Instance.Config.ActivatedMessage);
+                foreach (Room room in Room.List)
+                    room.Color = Color.cyan;
+                Timing.CallDelayed(180, () =>
                 {
-                    Sobrevivientes.IsGodModeEnabled = true;
-                    Timing.CallDelayed(2f, () =>
+                    foreach (Player Sobrevivientes in Player.List.Where(plr => plr.CurrentRoom.Name == "EZ_Shelter"))
                     {
-                        Sobrevivientes.IsGodModeEnabled = false;
-                        Sobrevivientes.EnableEffect(EffectType.Flashed, 2);
-                        Sobrevivientes.Position = new Vector3(-53, 988, -50);
-                        Sobrevivientes.EnableEffect(EffectType.Visuals939);
-                    });
-                }
-                foreach (Player Surface in Player.List.Where(plr => plr.Zone == ZoneType.Surface))
-                    Surface.Kill("Omega Warhead");
-                Warhead.Detonate();
-            });
-            Timing.CallDelayed(155, () =>
+                        Sobrevivientes.IsGodModeEnabled = true;
+                        Timing.CallDelayed(1f, () =>
+                        {
+                            Sobrevivientes.IsGodModeEnabled = false;
+                            Sobrevivientes.EnableEffect(EffectType.Flashed, 2);
+                            Sobrevivientes.Position = new Vector3(-53, 988, -50);
+                            Sobrevivientes.EnableEffect(EffectType.Visuals939, 5);
+                        });
+                        Timing.CallDelayed(1.5f, () =>
+                        {
+                            Timing.CallDelayed(0.2f, Warhead.Detonate);
+                        });
+                    }
+                    foreach (Player Muertos in Player.List)
+                        if (Muertos.CurrentRoom.Type != RoomType.EzShelter)
+                        {
+                            Muertos.Kill("Omega Warhead");
+                        }
+                    foreach (Room room in Room.List)
+                        room.Color = Color.blue;
+                });
+                Timing.CallDelayed(155, () =>
+                {
+                    //Map.Broadcast(10, "<color=blue> RESCUE HELICOPTER COMING TO SURFACE </color>");
+                    //RespawnEffectsController.ExecuteAllEffects(RespawnEffectsController.EffectType.Selection, SpawnableTeamType.NineTailedFox);
+                });
+                response = string.Empty;
+                return false;
+            }
+            else
             {
-                //Map.Broadcast(10, "<color=blue> HELICOPTERO DE RESCATE LLEGANDO A SURFACE </color>");
-                //RespawnEffectsController.ExecuteAllEffects(RespawnEffectsController.EffectType.Selection, SpawnableTeamType.NineTailedFox);
-            });
-            foreach (Room room in Room.List)
-                room.Color = Color.blue;
-            response = string.Empty;
-            return false;
+                response = "You need Warhead Events permissions to use this commands";
+                return true;
+            }
         }
     }
 }
