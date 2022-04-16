0282e22349e652f2cfcdc8dd7375c467f19daf43
Claudio <72409092+ClaudioPanConQueso@users.noreply.github.com>
2022-04-16 10:22:08 -0300

    Add files via upload

---
+++ SCPSL/EventHandlers.cs
@@ -0,0 +1,70 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using UnityEngine;
+using Map = Exiled.API.Features.Map;
+
+namespace OmegaWarheadPlugin
+{
+    class EventHandlers
+    {
+
+        public void OnWarheadStart(StartingEventArgs ev)
+        {
+            if(Plugin.Singleton.Config.ReplaceAlpha)
+            {
+                ev.IsAllowed = false;
+                OmegaWarhead();
+            }
+        }
+        public void OmegaWarhead()
+        {
+            foreach (Room room in Room.List)
+                room.Color = Color.cyan;
+            Cassie.Message(Plugin.Singleton.Config.Cassie, false, false);
+            Map.Broadcast(10, Plugin.Singleton.Config.ActivatedMessage);
+            Timing.CallDelayed(150, () =>
+            {
+                foreach (Door checkpoint in Door.List)
+                {
+                    if (checkpoint.Type == DoorType.CheckpointEntrance || checkpoint.Type == DoorType.CheckpointLczA || checkpoint.Type == DoorType.CheckpointLczB)
+                    {
+                        checkpoint.IsOpen = true;
+                        checkpoint.Lock(69420, DoorLockType.Warhead);
+                    }
+                }
+            });
+            var handle = Timing.CallDelayed(180, () =>
+            {
+                foreach (Player Sobrevivientes in Player.List)
+                {
+                    if (Sobrevivientes.CurrentRoom.Type == RoomType.EzShelter)
+                    {
+                        Sobrevivientes.IsGodModeEnabled = true;
+                        Timing.CallDelayed(Plugin.Singleton.Config.TimeToExplodeAfterCassie - 0.2f, () =>
+                        {
+                            Sobrevivientes.IsGodModeEnabled = false;
+                            Sobrevivientes.EnableEffect(EffectType.Flashed, 2);
+                            Sobrevivientes.Position = new Vector3(-53, 988, -50);
+                            Sobrevivientes.EnableEffect(EffectType.Visuals939, 5);
+                        });
+                    }
+                    Timing.CallDelayed(Plugin.Singleton.Config.TimeToExplodeAfterCassie, Warhead.Detonate);
+                }
+                foreach (Player Muertos in Player.List)
+                    if (Muertos.CurrentRoom.Type != RoomType.EzShelter)
+                    {
+                        Muertos.Kill("Omega Warhead");
+                    }
+                foreach (Room room in Room.List)
+                    room.Color = Color.blue;
+            });
+            Timing.CallDelayed(155, () =>
+            {
+                //Map.Broadcast(10, "<color=blue> RESCUE HELICOPTER COMING TO SURFACE </color>");
+                //RespawnEffectsController.ExecuteAllEffects(RespawnEffectsController.EffectType.Selection, SpawnableTeamType.NineTailedFox);
+            });
+        }
+    }
+}
--- SCPSL/Plugin.cs
+++ SCPSL/Plugin.cs
@@ -4,31 +4,32 @@ namespace OmegaWarheadPlugin
 {
     public class Plugin : Plugin<Config>
     {
-        private static readonly Plugin Singleton = new Plugin();
-        public static Plugin Instance => Singleton;
-
-        private PlayerE player;
+        public static Plugin Singleton;
+        internal EventHandlers handler;
 
         public override void OnEnabled()
         {
+            Singleton = this;
             RegisterEvents();
+            base.OnEnabled();
         }
-
         public override void OnDisabled()
         {
+            Singleton = null;
             UnregisterEvents();
+            base.OnDisabled();
         }
 
         public void RegisterEvents()
         {
-            player = new PlayerE();
-            Exiled.Events.Handlers.Warhead.Starting += player.OnWarheadStart;
+            handler = new EventHandlers();
+            Exiled.Events.Handlers.Warhead.Starting += handler.OnWarheadStart;
         }
 
         public void UnregisterEvents()
         {
-            Exiled.Events.Handlers.Warhead.Starting -= player.OnWarheadStart;
-            player = null;
+            Exiled.Events.Handlers.Warhead.Starting -= handler.OnWarheadStart;
+            handler = null;
         }
     }
 }
