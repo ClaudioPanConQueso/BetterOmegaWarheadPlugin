1c0156e3eb1bdf436169ac52b4e401ab5b4b4a32
Claudio <72409092+ClaudioPanConQueso@users.noreply.github.com>
2022-05-18 07:43:24 -0300

    Add files via upload

---
+++ SCPSL/Commands/Stop.cs
@@ -0,0 +1,27 @@
+﻿using CommandSystem;
+using System;
+
+namespace OmegaWarheadPlugin.Commands
+{
+    public class Stop : ICommand
+    {
+        public string Command { get; } = "stopomega";
+        public string[] Aliases { get; } = null;
+        public string Description { get; } = "stops the Omega Warhead.";
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            // TO-DO
+            if (sender.CheckPermission(PlayerPermissions.WarheadEvents))
+            {
+                //Cassie.Message("Omega Warhead detonation cancelled");
+                response = "Omega Warhead stopped.";
+                return false;
+            }
+            else
+            {
+                response = "You need Warhead Events permissions to use this commands";
+                return true;
+            }
+        }
+    }
+}
